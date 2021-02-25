44f96e462a08bb5dfee555d8d3a6aedd14552067
kdetienne <kelsey.detienne@mymail.mines.edu>
2021-02-25 16:45:57 -0700

    Added methods for TestBoard (some are still incomplete)

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -1,5 +1,36 @@
 package experiment;
 
-public class TestBoard {
+import java.util.HashSet;
+import java.util.Set;
 
+public class TestBoard {
+	Set <TestBoardCell> board;
+	Set <TestBoardCell> targets;
+
+	public TestBoard() {
+		super();
+		this.board = new HashSet <TestBoardCell>();
+	}
+
+	void calcTargets(TestBoardCell startCell, int pathlength) {
+		//int left = pathlength;
+		//targets = new HashSet<TestBoardCell>();
+		
+		
+	}
+	
+	
+	Set <TestBoardCell> getTargets(){
+		return targets;
+	}
+	
+	TestBoardCell getCell(int row, int col) {
+		TestBoardCell cell = new TestBoardCell(row,col);
+		for(TestBoardCell current : board) {
+			if(current.equals(cell)) {
+				return current;
+			}
+		}
+		return null;
+	}
 }
