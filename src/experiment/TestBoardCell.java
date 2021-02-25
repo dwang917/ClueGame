03ca2084f3e1e7216f67d8ef1e3e0a8057c88862
kdetienne <kelsey.detienne@mymail.mines.edu>
2021-02-25 14:09:18 -0700

    Set up methods in TestBoardCell

--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -1,5 +1,42 @@
 package experiment;
 
-public class TestBoardCell {
+import java.util.HashSet;
+import java.util.Set;
 
+public class TestBoardCell {
+	private int row;
+	private int col;
+	private Set <TestBoardCell> adjacent = new HashSet <TestBoardCell>();
+	
+	public TestBoardCell(int row, int col) {
+		super();
+		this.row = row;
+		this.col = col;
+	}
+	
+	void addAdjacency(TestBoardCell cell) {
+		adjacent.add(cell);
+	}
+	
+	Set <TestBoardCell> getAdjList(){
+		return adjacent;
+	}
+	
+	void setRoom(boolean b) {
+		
+	}
+	
+	void booleanIsRoom() {
+		
+	}
+	
+	void setOccupied(boolean b) {
+		
+	}
+	
+	boolean getOccupied() {
+		return false;
+		
+	}
+	
 }
