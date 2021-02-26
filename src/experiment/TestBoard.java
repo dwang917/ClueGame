bc1f14e8bc0ca43d9de38a184e66da6b47d7b858
kdetienne <kelsey.detienne@mymail.mines.edu>
2021-02-25 23:24:43 -0700

    Empty methods in TestBoard and TestBoardCell

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -1,54 +1,24 @@
 package experiment;
 
-import java.util.HashSet;
 import java.util.Set;
 
 public class TestBoard {
-	private int size = 4;
-	Set <TestBoardCell> board;
-	Set <TestBoardCell> targets;
 
 	public TestBoard() {
 		super();
-		this.board = new HashSet <TestBoardCell>();
-		for(int i = 0; i < size; i++) {
-			for(int j = 0; j<size; j++) {
-				board.add(new TestBoardCell(i,j));
-			}
-		}
-		for(TestBoardCell cell : board) {
-			setAdjacent(cell);
-		}
 	}
-	void setAdjacent(TestBoardCell cell) {
-			if(cell.getRow()+1 < size)
-				cell.addAdjacency(new TestBoardCell(cell.getCol(),cell.getRow()+1));
-			if(cell.getRow()-1 > -1)
-				cell.addAdjacency(new TestBoardCell(cell.getCol(),cell.getRow()-1));
-			if(cell.getCol()+1 < size)
-				cell.addAdjacency(new TestBoardCell(cell.getCol()+1,cell.getRow()));
-			if(cell.getCol()-1 > -1)
-				cell.addAdjacency(new TestBoardCell(cell.getCol()-1,cell.getRow()));
-
-		}
+	
 
 	void calcTargets(TestBoardCell startCell, int pathlength) {
-		//int left = pathlength;
-		//targets = new HashSet<TestBoardCell>();
+		
 	}
 	
 	
 	Set <TestBoardCell> getTargets(){
-		return targets;
+		return null;
 	}
 	
 	public TestBoardCell getCell(int row, int col) {
-		TestBoardCell cell = new TestBoardCell(row,col);
-		for(TestBoardCell current : board) {
-			if(current.equals(cell)) {
-				return current;
-			}
-		}
 		return null;
 	}
 }
--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -1,55 +1,36 @@
 package experiment;
 
-import java.util.HashSet;
 import java.util.Set;
 
 public class TestBoardCell {
-	private int row;
-	private int col;
-	private boolean room;
-	private boolean occupied;
-	private Set <TestBoardCell> adjacent = new HashSet <TestBoardCell>();
 	
 	public TestBoardCell(int row, int col) {
 		super();
-		this.row = row;
-		this.col = col;
 	}
 	
 
-	public void addAdjacency(TestBoardCell cell) {
-		adjacent.add(cell);
+	void addAdjacency(TestBoardCell cell) {
 	}
 	
-	public Set <TestBoardCell> getAdjList(){
-		return adjacent;
+	Set <TestBoardCell> getAdjList(){
+		return null;
 	}
 	
 	void setRoom(boolean b) {
-		room = b;
+		
 	}
 	
 	boolean IsRoom() {
-		return room;
+		return false;
 	}
 	
 	void setOccupied(boolean b) {
-		occupied = b;
+	
 	}
 	
 	boolean getOccupied() {
-		return occupied;
+		return false;
 		
-	}
-
-
-	public int getRow() {
-		return row;
-	}
-
-
-	public int getCol() {
-		return col;
 	}
 	
 }
