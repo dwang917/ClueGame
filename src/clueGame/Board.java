ce1df6ad0c60ab9b95efcb12f4da57e54fc78441
Daming Wang <damingwang@mymail.mines.edu>
2021-03-01 21:59:50 -0700

    Added stubs

---
+++ src/clueGame/Board.java
@@ -0,0 +1,31 @@
+package clueGame;
+
+import java.util.Map;
+
+public class Board {
+
+	private BoardCell[][] grid;
+	private int numRows;
+	private int numColumns;
+	private String layoutConfigFile;
+	private String setupConfigFile;
+	private Map<Character, Room> roomMap;
+	public static Board theInstance;
+	
+	public void initialize() {
+		
+	}
+	
+	public void loadConfigFiles() {
+		
+	}
+	
+	public void loadSetupConfig() {
+		
+	}
+	
+	public void loadLayoutConfig() {
+		
+	}
+	
+}
