de8e0bd86fc29b65c2f3e2109d819a211c593ada
kdetienne <kelsey.detienne@mymail.mines.edu>
2021-03-05 19:34:28 -0700

    more editing of config

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -77,7 +77,7 @@ public class Board {
 		int col = 0;
 		int row = 0;
 		String line;
-		Room newRoom;
+		Room newRoom = new Room();
 		
 		try {
 			FileReader obj2 = new FileReader(layoutConfigFile);
@@ -97,75 +97,81 @@ public class Board {
 			
 			for(row = 0; row < numRows; row++) {
 				line = boardStrings.get(row);
+				System.out.println();
+				System.out.println(line.replace(',', ' '));
+		
 				col = 0;
 				for(int charLoc = 0; charLoc < line.length(); charLoc++) {
 					if(line.charAt(charLoc) == ',' && col+1 < numCols) {
+						grid[row][col] = cell;
+						System.out.print(grid[row][col].getInitial() + " ");
 						col++;
 						cell = new BoardCell(row,col);
 						continue;
 					}
-					else if(charLoc+1 < line.length() && line.charAt(charLoc) == 'S' && line.charAt(charLoc+1) == 'O') {
-						cell.setSPassage('S');
-					}
-					else if(charLoc+1 < line.length() && line.charAt(charLoc) == 'O' && line.charAt(charLoc+1) == 'S') {
-						cell.setSPassage('O');
-					}
-					else if(charLoc+1 < line.length() && line.charAt(charLoc) == 'K' && line.charAt(charLoc+1) == 'M') {
-						cell.setSPassage('K');
-					}
-					else if(charLoc+1 < line.length() && line.charAt(charLoc) == 'M' && line.charAt(charLoc+1) == 'K') {
-						cell.setSPassage('M');
-					}
-					else if(line.charAt(charLoc) == '<' || line.charAt(charLoc) == '>' || line.charAt(charLoc) == 'v'||line.charAt(charLoc) == '^') {
-						cell.setDirection(line.charAt(charLoc));
-					}
 					else {
 						cell.setInitial(line.charAt(charLoc));
-						if(charLoc+1 < line.length() && line.charAt(charLoc+1) == '#') {
-							charLoc++;
-							cell.setLabel(true);
-						}
-						else if(charLoc+1 < line.length() && (line.charAt(charLoc+1) == '*' )){
-							charLoc++;
-							cell.setRoomCenter(true);
-						}
-						else if(charLoc+1 < line.length() && line.charAt(charLoc+1) != ',') {
-							charLoc++;
-							cell.setSPassage(line.charAt(charLoc));
-						}
-								if(cell.isRoomCenter()) {
-									
-									newRoom = roomMap.get(cell.getInitial());
-									newRoom.setCenterCell(cell);
-									roomMap.put(cell.getInitial(), newRoom);
-									
-									}
-								else if(cell.isLabel()) {
-									
-									newRoom = roomMap.get(cell.getInitial());
-									newRoom.setLabelCell(cell);
-									roomMap.put(cell.getInitial(), newRoom);
-									
-									}
-								
-						cell.setRoom(roomMap.get(line.charAt(charLoc)),line.charAt(charLoc));
-						
+						if(charLoc +1 < line.length()) {
+							if(line.charAt(charLoc+1) == '<' || line.charAt(charLoc+1) == '>' || line.charAt(charLoc+1) == 'v'||line.charAt(charLoc+1) == '^') {
+								cell.setDirection(line.charAt(charLoc));
+								charLoc++;
+							}
+							else if(line.charAt(charLoc+1) == '#') {
+								cell.setLabel(true);
+								charLoc++;
+							}
+							else if(line.charAt(charLoc+1) == '*' ){
+								cell.setRoomCenter(true);
+								charLoc++;
+							}
+							else if(line.charAt(charLoc) == 'S' && line.charAt(charLoc+1) == 'O') {
+								cell.setSPassage('S');
+								charLoc++;
+							}
+							else if(line.charAt(charLoc) == 'O' && line.charAt(charLoc+1) == 'S') {
+								cell.setSPassage('O');
+								charLoc++;
+							}
+							else if(line.charAt(charLoc) == 'K' && line.charAt(charLoc+1) == 'M') {
+								cell.setSPassage('K');
+								charLoc++;
+							}
+							else if(line.charAt(charLoc) == 'M' && line.charAt(charLoc+1) == 'K') {
+								cell.setSPassage('M');
+								charLoc++;
+							}
+						}	
 					}
-					
-				
-					this.grid[row][col] = cell;
-				
-					System.out.print(grid[row][col].getInitial() + " ");
+				}
+				System.out.println();
 
-				}
-				System.out.println();
 			}
-			for(int i = 0; i < numRows; i++) {
-				System.out.print(i);
-				for(int j = 0; j < numCols; j++) {
-					System.out.print(grid[i][j].getInitial());
+			//for(int i = 0; i < numRows; i++) {
+			//	System.out.print(i);
+			//	for(int j = 0; j < numCols; j++) {
+			//		System.out.print(grid[i][j].getInitial());
+			//	}
+			//	System.out.println();
+			//}
+			
+			for(row = 0; row < numRows; row++) {
+				for(col = 0; col < numCols; col++) {
+					if(grid[row][col].isRoomCenter()) {
+						
+						newRoom = roomMap.get(grid[row][col].getInitial());
+						newRoom.setCenterCell(grid[row][col]);
+						roomMap.put(grid[row][col].getInitial(), newRoom);
+						
+						}
+					else if(grid[row][col].isLabel()) {
+						
+						newRoom = roomMap.get(grid[row][col].getInitial());
+						newRoom.setLabelCell(grid[row][col]);
+						roomMap.put(grid[row][col].getInitial(), newRoom);
+						
+						}
+					grid[row][col].setRoom(newRoom, grid[row][col].getInitial());
 				}
-				System.out.println();
 			}
 		} catch(FileNotFoundException e) {
 			e.printStackTrace();
@@ -183,14 +189,6 @@ public class Board {
 		
 		loadLayoutConfig();
 		
-		for(int row = 0; row < numRows; row++) {
-			System.out.print(row);
-			for(int col = 0; col < numCols; col++) {
-				System.out.print("\""+row + "," +col + "\""+ " ");
-				System.out.print(grid[row][col].getInitial());
-			}
-			System.out.println();
-		}
 
 	}
 	
